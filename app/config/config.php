1a5af013bd03b50490adad270a08b36f7b154737
rafique129 <55715890+rafique129@users.noreply.github.com>
2020-12-14 02:08:07 +0000

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'shareworld');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/shareworld/');
+  // Site Name
+  define('SITENAME', 'Shareworld');
+  // App Version
+  define('APPVERSION','1.0.0');
